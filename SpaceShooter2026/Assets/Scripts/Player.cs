cccb0842a4d95c9d0aee09665ec670ce5bc0a8ac
Kevin Cherry <kcherr1@gmail.com>
2026-03-12 19:36:20 -0500

    player can move up/down and limits y movement

---
+++ SpaceShooter2026/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+  private SpaceShooterInputActions inputActions;
+  public float speed = 0.1f;
+  private const float Y_LIMIT = 4.6f;
+
+  private void Start() {
+    inputActions = new();
+    inputActions.Enable();
+    inputActions.Standard.Enable();
+  }
+
+  private void Update() {
+    if (inputActions.Standard.MoveUp.IsPressed()) {
+      this.transform.Translate(Vector3.up * speed);
+    }
+    else if (inputActions.Standard.MoveDown.IsPressed()) {
+      this.transform.Translate(Vector3.down * speed);
+    }
+    if (this.transform.position.y > Y_LIMIT) {
+      this.transform.position = new Vector3(transform.position.x, Y_LIMIT);
+    }
+    else if (this.transform.position.y < -Y_LIMIT) {
+      this.transform.position = new Vector3(transform.position.x, -Y_LIMIT);
+    }
+  }
+}
