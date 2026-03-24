19df1346aa5eca6f745bb32201d58a273d35bbae
LevitatingSock <159564663+LevitatingSock@users.noreply.github.com>
2026-03-23 17:38:54 -0700

    Revert "New Enemies and Progressive Difficulty"
    This reverts commit 0263b6706d36d5ec5b49027ca0a7ed864dd54ed6.

--- SpaceShooter2026/Assets/Scripts/Enemy.cs
+++ SpaceShooter2026/Assets/Scripts/Enemy.cs
@@ -3,36 +3,9 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
   // set in inspector
   public float speed;
-<<<<<<< Updated upstream
-=======
-  public bool isHoming;
-  public bool isBobber;
-  public GameObject expoPrefab;
->>>>>>> Stashed changes
 
-  private Transform player;
-  private float height = 0.05f;
-
-    private void Start()
-    {
-        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
-        player = playerObj.transform;
-    }
-
-    void Update() {
-    if (!isHoming && !isBobber)
-        {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
-        }
-    else if (isHoming)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-        }
-    else if (isBobber)
-        {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
-            transform.position = new Vector3(transform.position.x, transform.position.y + (Mathf.Sin(Time.time * speed) * height), transform.position.z);
-        }
+  void Update() {
+    transform.Translate(Vector3.left * speed * Time.deltaTime);
   }
 
   private void OnCollisionEnter2D(Collision2D c) {
--- SpaceShooter2026/Assets/Scripts/Game.cs
+++ SpaceShooter2026/Assets/Scripts/Game.cs
@@ -1,18 +1,13 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Game : MonoBehaviour {
   // set in inspector
   public float enemySpawnDelay;
   public float switchDelay;
-  public float despawnTime;
-    //public GameObject enemyPrefab;
-    //public GameObject fastEnemyPrefab;
-    public GameObject[] enemyPrefabs;
+  public GameObject enemyPrefab;
   public GameObject powerupPrefab;
   public BoxCollider2D spawnRangeRight;
   public BoxCollider2D spawnRangeLeft;
-  public Score gameScore;
 
   // private fields
   private float powerUpDelay;
@@ -21,88 +16,57 @@ public class Game : MonoBehaviour {
   private BoxCollider2D spawnRange;
   private float switchTimer;
   private Quaternion spawnDirection;
-  private int threshold = 1000;
 
-    private void Start() {
+  private void Start() {
     powerUpDelay = Random.Range(5f, 10f);
     powerupSpawnTimer = 0;
     spawnRange = spawnRangeRight;
-    }
+  }
 
   private void SpawnEnemy() {
     Vector3 enemySpawnPt = new Vector3(
         Random.Range(spawnRange.bounds.min.x, spawnRange.bounds.max.x),
         Random.Range(spawnRange.bounds.min.y, spawnRange.bounds.max.y),
         0);
-    GameObject enemy = Instantiate(enemyPrefabs[UnityEngine.Random.Range(0, enemyPrefabs.Length)], enemySpawnPt, spawnDirection);
-    Destroy(enemy, despawnTime);
+    Instantiate(enemyPrefab, enemySpawnPt, spawnDirection);
   }
   private void SpawnPowerup() {
     Vector3 powerupSpawnPt = new Vector3(
         Random.Range(spawnRange.bounds.min.x, spawnRange.bounds.max.x),
         Random.Range(spawnRange.bounds.min.y, spawnRange.bounds.max.y),
         0);
-    GameObject shieldBoost = Instantiate(powerupPrefab, powerupSpawnPt, spawnDirection);
-    Destroy(shieldBoost, despawnTime);
+    Instantiate(powerupPrefab, powerupSpawnPt, spawnDirection);
   }
-<<<<<<< Updated upstream
   void Update() {
     // check spawn enemy
     enemySpawnTimer += Time.deltaTime;
     if (enemySpawnTimer >= enemySpawnDelay) {
       SpawnEnemy();
       enemySpawnTimer = 0.0f;
-=======
-  private void levelIncrease()
-    {
-        enemySpawnDelay -= 0.1f;
->>>>>>> Stashed changes
     }
 
-    void Update()
-    {
-        //if (!ui.IsReady) {
-        //  return;
-        //}
+    // check spawn powerup
+    powerupSpawnTimer += Time.deltaTime;
+    if (powerupSpawnTimer >= powerUpDelay) {
+      SpawnPowerup();
+      powerUpDelay = Random.Range(5, 10);
+      powerupSpawnTimer = 0.0f;
+    }
 
-        // check spawn enemy
-        enemySpawnTimer += Time.deltaTime;
-        if (enemySpawnTimer >= enemySpawnDelay)
+    switchTimer += Time.deltaTime;
+    if (switchTimer > switchDelay)
         {
-            SpawnEnemy();
-            enemySpawnTimer = 0.0f;
-        }
-
-        // check spawn powerup
-        powerupSpawnTimer += Time.deltaTime;
-        if (powerupSpawnTimer >= powerUpDelay)
-        {
-            SpawnPowerup();
-            powerUpDelay = Random.Range(5, 10);
-            powerupSpawnTimer = 0.0f;
-        }
-
-        switchTimer += Time.deltaTime;
-        if (switchTimer > switchDelay)
-        {
+            Debug.Log("15 seconds passed");
             if (spawnRange == spawnRangeRight)
             {
                 spawnRange = spawnRangeLeft;
                 spawnDirection = Quaternion.Euler(0f, 180f, 0f);
-            }
-            else
+            } else
             {
                 spawnRange = spawnRangeRight;
                 spawnDirection = Quaternion.identity;
             }
             switchTimer = 0;
         }
-
-        if (gameScore.score >= threshold)
-        {
-            levelIncrease();
-            threshold += 1000;
-        }
-
-    }
+  }
 }
